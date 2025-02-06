a0c5770390fd18eee8fe3de33acedaa51224608f
Felipe José Meneguetti <flmeneguetti@yahoo.com.br>
2025-02-05 21:50:01 -0300

    aaa

--- terraform-oci-volume/main.tf
+++ terraform-oci-volume/main.tf
@@ -10,13 +10,12 @@ terraform {
 
 #Declara o volume
 resource "oci_core_volume" "data_volume" {
-  compartment_id       = var.compartimento_ocid
-  availability_domain  = var.instance_availability_domain
-  display_name         = var.volume_display_name
-  is_auto_tune_enabled = "true"
-  size_in_gbs          = var.size_in_gbs_volume
-  vpus_per_gb          = var.vpus_per_gbs_volume
-  freeform_tags        = var.tags_freeform
+  compartment_id      = var.compartimento_ocid
+  availability_domain = var.instance_availability_domain
+  display_name        = var.volume_display_name
+  size_in_gbs         = var.size_in_gbs_volume
+  vpus_per_gb         = var.vpus_per_gbs_volume
+  freeform_tags       = var.tags_freeform
 }
 
 #Anexa o volume à instância
@@ -25,7 +24,7 @@ resource "oci_core_volume_attachment" "volume_attachment" {
   instance_id     = var.instance_ocid
   volume_id       = oci_core_volume.data_volume.id
 
-  display_name                        = "attach-${var.volume_display_name}"
+  display_name                        = var.volume_display_name
   is_pv_encryption_in_transit_enabled = "true"
   is_read_only                        = "false"
   is_shareable                        = "true"
--- terraform-oci-volume/outputs.tf
+++ terraform-oci-volume/outputs.tf
@@ -1,4 +1,4 @@
 output "volume_id" {
-  value = oci_core_volume.data_volume.id
+  value       = oci_core_volume.data_volume.id
   description = "OCID do volume criado."
 }
